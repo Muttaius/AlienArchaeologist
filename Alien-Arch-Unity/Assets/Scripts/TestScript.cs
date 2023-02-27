090f56d96172c5b81aebe1f1d58777eb6496d7c0
Muttaius <keithdmorrison1@gmail.com>
2023-02-27 15:46:04 +0000

    Coded in for and if loops

--- Alien-Arch-Unity/Assets/Scripts/TestScript.cs
+++ Alien-Arch-Unity/Assets/Scripts/TestScript.cs
@@ -8,15 +8,87 @@ public class TestScript : MonoBehaviour
     public string startMessage;
     public string updateMessage;
 
+    public string message1;
+    public string message2;
+    public string message3;
+
+    public int messagechoice;
+
+    public string[] messages;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.LogWarning(startMessage);
+        Debug.Log(startMessage);
+
+        /*
+        if (messagechoice == 1)
+        {
+            Debug.Log(message1);
+        }
+
+        else if (messagechoice == 2)
+        {
+            Debug.Log(message2);
+        }
+
+        else if (messagechoice == 3)
+        {
+            Debug.Log(message3);
+        }
+
+        else
+        {
+            Debug.Log("Invalid message choice");
+        }
+        */
+
+
+
+        /* if (messagechoice == 1)
+        {
+            Debug.Log(messages[0]);
+        }
+
+        else if (messagechoice == 2)
+        {
+            Debug.Log(messages[1]);
+        }
+
+        else if (messagechoice == 3)
+        {
+            Debug.Log(messages[2]);
+        }
+
+        else
+        {
+            Debug.Log("Invalid message choice");
+        } */
+
+        // messagechoice = Random.Range(1, 4);
+
+
+        /*if (messagechoice <= messages.Length && messagechoice >= 1)
+            {
+                Debug.Log(messagechoice - 1);
+            }
+
+            else
+            {
+                Debug.Log("Invalid message choice");
+            }*/
+
+        for (int i = 0; i < messages.Length; ++i)
+
+        {
+            Debug.Log(messages[i]);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.LogError(updateMessage);
+        Debug.Log(updateMessage);
     }
 }
